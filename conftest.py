3acaede7c5e9800b3fd85e509f31628af8b16f62
Ram Tiwari <tiwari.ram@thinksys.com>
2026-03-29 16:56:58 +0530

    create button lo

--- conftest.py
+++ conftest.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def browser():
     options = Options()
     options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=options)
     yield driver
-   # driver.quit()
+    driver.quit()
